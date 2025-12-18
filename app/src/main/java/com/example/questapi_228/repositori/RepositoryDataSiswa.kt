b1d98403813bae68a590dbd1b4aa1c5ffd293b32
MannantaB <mannanta18@gmail.com>
2025-12-18 09:24:31 +0700

    add jaringan repository untuk data siswa

--- app/src/main/java/com/example/questapi_228/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_228/repositori/RepositoryDataSiswa.kt
@@ -1,9 +1,18 @@
 package com.example.questapi_228.repositori
 
 import android.provider.ContactsContract
+import com.example.questapi_228.apiservice.ServiceApiSiswa
 import com.example.questapi_228.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
