bf6390a61ea22074ec6c4fbdd668f6c47f36d108
MannantaB <mannanta18@gmail.com>
2025-12-18 09:15:50 +0700

    add DataSiswa model for API response

---
+++ app/src/main/java/com/example/questapi_228/modeldata/DataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questapi_228.modeldata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
