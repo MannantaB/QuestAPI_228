0571143e1043a78841df5a151254be00155c0c58
MannantaB <mannanta18@gmail.com>
2025-12-25 20:33:22 +0700

    add methods for single student retrieval, editing, and deletion in RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_228/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_228/repositori/RepositoryDataSiswa.kt
@@ -8,6 +8,9 @@ import retrofit2.Response
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusDataSiswa(id: Int): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -15,4 +18,7 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusDataSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSiswa(id)
 }
