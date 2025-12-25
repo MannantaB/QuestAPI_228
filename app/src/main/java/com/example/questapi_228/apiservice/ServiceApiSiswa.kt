78b22211d9eed8fa2b16b0776d71f102876c7b54
MannantaB <mannanta18@gmail.com>
2025-12-25 20:24:07 +0700

    add CRUD operations to ServiceApiSiswa

--- app/src/main/java/com/example/questapi_228/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_228/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,11 @@ package com.example.questapi_228.apiservice
 
 import com.example.questapi_228.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
@@ -11,4 +14,13 @@ interface ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("baca1Teman.php")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun putSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa) :retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSiswa(@Query("id") id: Int) :retrofit2.Response<Void>
 }
