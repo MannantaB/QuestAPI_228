26fef2080aef97daf45aefb7afa9ca8452d0db7e
MannantaB <mannanta18@gmail.com>
2025-12-25 21:12:16 +0700

    add DetailViewModel implementation to manage student detail loading state

--- app/src/main/java/com/example/questapi_228/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_228/viewmodel/DetailViewModel.kt
@@ -2,12 +2,26 @@
 
 package com.example.questapi_228.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi_228.modeldata.DataSiswa
+import com.example.questapi_228.repositori.RepositoryDataSiswa
+import com.example.questapi_228.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val statusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+    RepositoryDataSiswa): ViewModel(){
+        private val idSiswa: Int =
+            checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+    }
 
 
