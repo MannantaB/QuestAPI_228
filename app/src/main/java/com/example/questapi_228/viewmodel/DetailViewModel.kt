6970955b75e7fba18a5e16a12fdaaba42f817e5c
MannantaB <mannanta18@gmail.com>
2025-12-25 21:18:06 +0700

    add getSatuSiswa method to DetailViewModel for fetching student details with error handling

--- app/src/main/java/com/example/questapi_228/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_228/viewmodel/DetailViewModel.kt
@@ -7,12 +7,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_228.modeldata.DataSiswa
 import com.example.questapi_228.repositori.RepositoryDataSiswa
 import com.example.questapi_228.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 sealed interface StatusUIDetail {
-    data class Success(val statusiswa: DataSiswa) : StatusUIDetail
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
@@ -22,6 +25,26 @@ class DetailViewModel(savedStateHandle: SavedStateHandle, private val repository
             checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
+
+    init {
+        getSatuSiswa()
     }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            }
+            catch (e: Exception){
+                StatusUIDetail.Error
+            }
+            catch (e: HttpException){
+                StatusUIDetail.Error
+            }
+       }
+    }
+    }
+
 
 
