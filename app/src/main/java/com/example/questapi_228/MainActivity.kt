e453415e77058155910f20814daf1504f6069cd4
MannantaB <mannanta18@gmail.com>
2025-12-25 22:01:00 +0700

    refactor: replace Greeting composable with DataSiswaApp in MainActivity

--- app/src/main/java/com/example/questapi_228/MainActivity.kt
+++ app/src/main/java/com/example/questapi_228/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.questapi_228
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+//import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_228.ui.theme.QuestAPI_228Theme
+import com.example.questapi_228.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +23,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPI_228Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_228Theme {
-        Greeting("Android")
     }
 }
